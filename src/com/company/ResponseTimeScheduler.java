bd0527ac936eded77ccfdcdbe6a458368ada9986
jh3618-kim <jh3618.kim@samsung.com>
2021-01-07 23:07:19 +0900

    싱글톤 패턴 적용으로 동일객체

--- src/com/company/ResponseTimeScheduler.java
+++ src/com/company/ResponseTimeScheduler.java
@@ -1,6 +1,17 @@
 package com.company;
 
 public class ResponseTimeScheduler implements ElevatorScheduler{
+    private static ElevatorScheduler scheduler;
+
+    private ResponseTimeScheduler() {
+    }
+
+    public static ElevatorScheduler getInstance() {
+        if (scheduler == null) {
+            scheduler = new ResponseTimeScheduler();
+        }
+        return scheduler;
+    }
 
     @Override
     public int selectElevator(ElevatorManager manager, int destination, Direction direction) {
