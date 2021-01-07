6503cd815320dea1c65e24c0e8329226f542aaa5
jh3618-kim <jh3618.kim@samsung.com>
2021-01-08 00:59:41 +0900

    컴포지트 패턴 적용전 문제상황

---
+++ src/com/company/Body.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Body {
+    private int price;
+    private int power;
+
+    public Body(int price, int power) {
+        this.price = price;
+        this.power = power;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getPower() {
+        return power;
+    }
+}
