4b21c2fe35b2e0bf89e0f5ff2cff4192e5fd90cf
jh3618-kim <jh3618.kim@samsung.com>
2021-01-07 21:15:08 +0900

    add LGMotor

---
+++ src/com/company/LGMotor.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class LGMotor {
+    private Door door;
+    private MotorStatus motorStatus;
+
+    public LGMotor(Door door) {
+        this.door = door;
+        motorStatus = MotorStatus.STOPPED;
+    }
+
+    private void moveLGMotor(Direction direction) {
+        System.out.println("Move LG Motor : " + direction);
+    }
+
+    public MotorStatus getMotorStatus() {
+        return motorStatus;
+    }
+
+    public void setMotorStatus(MotorStatus motorStatus) {
+        this.motorStatus = motorStatus;
+    }
+
+    public void move(Direction direction) {
+        MotorStatus motorStatus = getMotorStatus();
+        if (motorStatus == MotorStatus.MOVING) return;
+
+        DoorStatus doorStatus = door.getDoorStatus();
+        if (doorStatus == DoorStatus.OPENED) door.close();
+
+        moveLGMotor(direction);
+        setMotorStatus(MotorStatus.MOVING);
+    }
+}
