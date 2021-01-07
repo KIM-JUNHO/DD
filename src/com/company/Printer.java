4bdc86a95230652beaa1f8c22457c3e10c871453
jh3618-kim <jh3618.kim@samsung.com>
2021-01-07 23:50:14 +0900

    static 변수로하면 해결되지만 계속 메모리상주 단점

--- src/com/company/Printer.java
+++ src/com/company/Printer.java
@@ -1,20 +1,12 @@
 package com.company;
 
 public class Printer {
-    private static Printer printer = null;
+    private static Printer printer = new Printer();
 
     private Printer() {
     }
 
     public static Printer getPrinter() {
-        if (printer == null) {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-            }
-
-            printer = new Printer();
-        }
         return printer;
     }
 
